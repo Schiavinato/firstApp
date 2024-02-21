0802078eb79f2f7ca80b98f2185601a93b4c5864
Schiavinato <125682014+Schiavinato@users.noreply.github.com>
2024-02-21 13:47:51 -0300

    a

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 30, fontWeight: 'bold', color:'blue', textAlign: 'right'}}>Nicolas Schiavinato</Text>
+      <Text style={[styles.text, { fontSize: 25, border: 'solid', borderWidth: 1, borderColor: 'black', textAlign: 'left', fontStyle: 'italic'}]}>Número 29</Text>
+      <Text style={{ backgroundColor: 'pink', fontSize: 20, textAlign: 'center'}}>Sala 7</Text>
+      <Text style={{ backgroundColor: 'red', fontSize: 20, textAlign: 'center', border: 'solid', borderWidth: 1, borderColor: 'black',}}>I love horses</Text>
     </View>
   );
 }
@@ -14,7 +16,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+  
+  }
 });
